c2c46b0acf5ebe2e65ef522eb35d87cdbbc4eda1
poly000 <1348292515a@gmail.com>
2024-07-02 16:10:25 +0800

    log: set logger to file and terminal

--- src/blive_qq_forward/__main__.py
+++ src/blive_qq_forward/__main__.py
@@ -4,9 +4,12 @@ import http.cookies
 import logging
 
 import aiohttp
+from bilibili_api import CredentialNoSessdataException, login, user
 
 import blivedm
 import blivedm.models.web as web_models
+
+logger = logging.getLogger(__package__)
 
 # 直播间ID的取值看直播间URL
 TEST_ROOM_IDS = [
@@ -19,8 +22,26 @@ TEST_ROOM_IDS = [
 
 
 async def main():
+    logging.basicConfig(filename=f'{__package__}.log', level=logging.INFO, encoding="utf-8")
+    logger.addHandler(logging.StreamHandler())
+
     # 这里填一个已登录账号的cookie的SESSDATA字段的值。不填也可以连接，但是收到弹幕的用户名会打码，UID会变成0
     sessdata = ""
+
+    if not sessdata:
+        try:
+            cred = login.login_with_qrcode()
+        except CredentialNoSessdataException:
+            cred = None
+
+        if cred is None or cred.sessdata is None:
+            print("登录失败！")
+        else:
+            sessdata = cred.sessdata
+    
+    user_info = await user.get_self_info(credential=cred)
+    print(f"登陆成功！欢迎回来，{user_info["name"]}")
+
     session = init_session(sessdata=sessdata)
     try:
         await run_multi_clients(session=session)
@@ -69,31 +90,30 @@ class MyHandler(blivedm.BaseHandler):
     def _on_heartbeat(
         self, client: blivedm.BLiveClient, message: web_models.HeartbeatMessage
     ):
-        logging.info("[%d] 心跳包", client.room_id)
-        logging.debug("[%d] %s", client.room_id, str(message))
+        logger.debug("[%d] %s", client.room_id, str(message))
 
     def _on_danmaku(
         self, client: blivedm.BLiveClient, message: web_models.DanmakuMessage
     ):
-        logging.info("[%d]-弹幕 %s：%s", client.room_id, message.uname, message.msg)
+        logger.info("[%d]-弹幕 %s：%s", client.room_id, message.uname, message.msg)
 
     def _on_gift(self, client: blivedm.BLiveClient, message: web_models.GiftMessage):
-        print(
+        log = (
             f"[{client.room_id}] {message.uname} 赠送{message.gift_name}x{message.num}"
-            f" （{message.coin_type}瓜子x{message.total_coin}）"
-        )
+            f" （{message.coin_type}瓜子x{message.total_coin}）")
+        logger.info(log)
 
     def _on_buy_guard(
         self, client: blivedm.BLiveClient, message: web_models.GuardBuyMessage
     ):
-        print(f"[{client.room_id}] {message.username} 购买{message.gift_name}")
+        log = f"[{client.room_id}] {message.username} 购买{message.gift_name}"
+        logger.info(log)
 
     def _on_super_chat(
         self, client: blivedm.BLiveClient, message: web_models.SuperChatMessage
     ):
-        print(
-            f"[{client.room_id}] 醒目留言 ¥{message.price} {message.uname}：{message.message}"
-        )
+        log = f"[{client.room_id}] 醒目留言 ¥{message.price} {message.uname}：{message.message}"
+        logger.info(log)
 
 
 if __name__ == "__main__":
